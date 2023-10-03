eb2125c2046ff4dcb1aa4deac7e72aed78b2c33f
Mani Poorna <ndmprao2001@gmail.com>
2023-10-03 17:58:24 +0530

    Adding Files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes,Route } from 'react-router-dom';
 import Home from './Components/Home.js';
 import History from './Components/History.js';
 import Navbar from './Components/Navbar.js';
@@ -7,13 +8,11 @@ import Navbar from './Components/Navbar.js';
 const App = () => {
   return (
     <div>
-      <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/reduxDictionary' element={<Home />} />
           <Route path='/history' element={<History />} />
         </Routes>
-      </Router>
     </div>
   );
 }
